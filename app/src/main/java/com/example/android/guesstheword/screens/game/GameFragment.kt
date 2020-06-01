858a4e88e15dd4bea98bb0a42d2e23e31dcbbbed
htl-leonding <aenderemich12345>
2020-06-01 22:49:51 +0200

    L05-E08 Create the GameViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,11 +17,14 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -30,6 +33,8 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
 
     // The current word
     private var word = ""
@@ -53,6 +58,10 @@ class GameFragment : Fragment() {
                 false
         )
 
+        Log.i("GameFragment", "Called ViewModelProvider")
+        // ViewModelProviders is deprecated https://stackoverflow.com/a/57427078
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
         resetList()
         nextWord()
 
@@ -61,7 +70,6 @@ class GameFragment : Fragment() {
         updateScoreText()
         updateWordText()
         return binding.root
-
     }
 
     /**
